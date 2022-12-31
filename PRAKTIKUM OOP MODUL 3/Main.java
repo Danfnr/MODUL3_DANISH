3776096300fcb29f6c1e97bdcd61e11b56750c79
Danfnr <116441092+Danfnr@users.noreply.github.com>
2022-12-31 13:40:02 +0700

    Add files via upload

---
+++ PRAKTIKUM OOP MODUL 3/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main (String s, int c, String t,){
+    System.out.printIn("===Menu===");
+    System.out.printIn("Select Menu:");
+    Square = s;
+    Circle = c;
+    Trapezoid = t;
+}
